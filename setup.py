8776f070f79a2c42157d88ffc056cf6b450b0451
William Belle <william.belle@gmail.com>
2021-02-19 16:30:56 +0100

    Add project urls in metadata (#10)

--- setup.py
+++ setup.py
@@ -7,11 +7,12 @@ import setuptools
 
 long_description = io.open("README.md", encoding="utf-8").read()
 version = __import__("django_epflmisc").__version__
+source_url = "https://github.com/epfl-si/django-epfl-misc"
 
 setuptools.setup(
     name="django-epfl-misc",
     version=version,
-    url="https://github.com/epfl-si/django-epfl-misc",
+    url=source_url,
     author="William Belle",
     author_email="william.belle@gmail.com",
     description="A Django application with helper functions and utilities.",
@@ -22,6 +23,11 @@ setuptools.setup(
     install_requires=[],
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
     zip_safe=False,
+    project_urls={
+        "Changelog": source_url + "/blob/main/CHANGELOG.md",
+        "Source": source_url,
+        "Tracker": source_url + "/issues",
+    },
     classifiers=[
         "Environment :: Web Environment",
         "Framework :: Django",
