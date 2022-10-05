f712ab424d960d298fbd29eae7a7a94adcd65fe0
William Belle <william.belle@gmail.com>
2022-10-05 15:21:10 +0200

    Drop Python 3.5 support (#38)
    Close: #37

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
     license="MIT",
     packages=setuptools.find_packages(),
     install_requires=[],
-    python_requires=">=3.5",
+    python_requires=">=3.6",
     zip_safe=False,
     project_urls={
         "Changelog": source_url + "/blob/main/CHANGELOG.md",
@@ -38,7 +38,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
